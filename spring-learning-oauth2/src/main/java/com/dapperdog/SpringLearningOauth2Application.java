6ee3fd2a01f753b8817ebd79e145e4b13fbc7cae
Alexander Zacharuk <zacharukalexander@gmail.com>
2017-02-12 18:20:37 -0500

    Initial commit for messing around with some spring stuff.

---
+++ spring-learning-oauth2/src/main/java/com/dapperdog/SpringLearningOauth2Application.java
@@ -0,0 +1,12 @@
+package com.dapperdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningOauth2Application.class, args);
+	}
+}
