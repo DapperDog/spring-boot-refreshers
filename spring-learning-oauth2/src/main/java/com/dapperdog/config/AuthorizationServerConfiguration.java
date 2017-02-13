c72bdd2c1b52283002e3a033b704cb9a1dd758ac
dapperdog <zacharukalexander@gmail.com>
2017-02-12 21:23:14 -0500

    using jwt token store instead, messing around with client using client_credentials only

--- spring-learning-oauth2/src/main/java/com/dapperdog/config/AuthorizationServerConfiguration.java
+++ spring-learning-oauth2/src/main/java/com/dapperdog/config/AuthorizationServerConfiguration.java
@@ -1,6 +1,7 @@
 package com.dapperdog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,11 +10,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+//if auth server is in seperate app, then token converters would need to be implemented identically in both spots
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+	
+	@Value("signing-key:8j12893j1289d8jf89j1892jf89j98a-sd0f")
+	private String signingKey;
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
@@ -21,30 +27,36 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 	public AuthorizationServerConfiguration(){
 		super();
 	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter(){
+		final JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey(signingKey);
+		return accessTokenConverter;
+	}
 	
 	@Bean
 	public TokenStore tokenStore(){
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 	
 	public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
-		endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+		endpoints.tokenStore(tokenStore())
+		.authenticationManager(authenticationManager)
+		.accessTokenConverter(accessTokenConverter());
 	}
 	
 	@Override
 	public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-		.withClient("user")
+		.withClient("client-with-secret")
 		.secret("usersecret")
-		.authorizedGrantTypes("password")
-		.scopes("webapp")
-		.autoApprove("webapp")
+		.authorizedGrantTypes("client_credentials","password")
+		.authorities("ROLE_CLIENT_WITH_SECRET")
+		.scopes("read")
 		.accessTokenValiditySeconds(3600).and()
-		.withClient("admin")
-		.secret("adminsecret")
-		.authorizedGrantTypes("password")
-		.scopes("webapp")
-		.autoApprove("webapp")
+		.withClient("admin-trusted")
+		.authorizedGrantTypes("password","authorization_code","refresh_token","implicit")
 		.accessTokenValiditySeconds(3600);
 	}
 }
